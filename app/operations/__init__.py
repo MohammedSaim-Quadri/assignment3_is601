aeafe2962597ca6285ba07bffde85a540450988a
Mohammed Saim <mohammedsaimquadri@gmail.com>
2025-09-20 18:12:48 -0400

    added docstring to operation functions

--- app/operations/__init__.py
+++ app/operations/__init__.py
@@ -2,20 +2,65 @@
 
 class Operations:
     # we use static methods so that we can use them without having to create instance of the class.
+    """
+    A class to encapsulate static methods for basic arithmetic operations.
+    Using static methods allows these functions to be called without creating an instance of the class.
+    """
     @staticmethod
     def addition(a:float, b:float) -> float:
+        """
+        Calculates the sum of two numbers.
+
+        Args:
+            a (float): The first number.
+            b (float): The second number.
+
+        Returns:
+            float: The sum of a and b.
+        """
         return a+b
     
     @staticmethod
     def subtraction(a:float, b:float) -> float:
+        """
+        Subtracts the second number from the first.
+        Args:
+            a (float): The first number.
+            b (float): The second number.
+
+        Returns:
+            float: The result of a - b.
+        """
         return a-b
     
     @staticmethod
     def multiplication(a:float, b:float) -> float:
+        """
+        Multiplies two numbers.
+        Args:
+            a (float): The first number.
+            b (float): The second number.
+        Returns:
+            float: The product of a and b.
+        
+        """
         return a*b
     
     @staticmethod
     def division(a:float, b:float) -> float:
+        """
+        Divides the first number by the second.
+
+        Args:
+            a (float): The dividend.
+            b (float): The divisor.
+
+        Returns:
+            float: The result of the division.
+
+        Raises:
+            ValueError: If the divisor 'b' is zero.
+        """
         if b == 0:
             # check to see if b is zero to avoid divide by zero error.
             raise ValueError("Division by zero is not allowed.")
